81a0cb30e0744b0c0b7c54ec0d1c5700946d8d33
Jason McCarver <slam@parasite.cc>
2016-10-07 09:35:41 -0500

    Version bump

--- lib/Parallel/ForkManager/Scaled.pm
+++ lib/Parallel/ForkManager/Scaled.pm
@@ -4,7 +4,7 @@ use namespace::clean;
 use Unix::Statgrab;
 use List::Util qw( min max );
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 extends 'Parallel::ForkManager';
 
@@ -219,7 +219,7 @@ Parallel::ForkManager::Scaled - Run processes in parallel based on CPU usage
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =head1 SYNOPSIS
 
